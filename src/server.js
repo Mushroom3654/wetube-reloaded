0c192c6554ee54aa559953cb346cf6524a7216f3
kby3654 <redsky__7@naver.com>
2021-05-31 21:59:05 +0900

    ADD: start express

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+    return res.send("Yeeeee");
+}
+
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`Server listening on port http://localhost${PORT}`);
+
+app.listen(PORT, handleListening);
+
+
+
+
