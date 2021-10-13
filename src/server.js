2d6306afae270d9af778509d97de765c90c7ad8d
kby3654 <redsky__7@naver.com>
2021-10-13 21:29:35 +0900

    Install Morgan

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
+// Morgan - Global Middleware
+const logger = morgan('common') // dev | combined | common | short | tiny
 
 const handleHome = (req, res) => {
-    return res.send("Yeeeee");
-}
+    return res.send("Hello");
+};
 
+const handleLogin = (req, res) => {
+    return res.send('Login Here');
+};
+
+app.use(logger);
 app.get("/", handleHome);
+app.get('/login', handleLogin);
 
-const handleListening = () => console.log(`Server listening on port http://localhost${PORT}`);
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
-
-
-
-
